42f70b7fdddec6f52f9a697ee5ad371ab906a693
Sergey <Maxxmell@yandex.ru>
2023-01-21 23:00:14 +0300

    coaches added

--- source/js/modules/modals/init-sliders.js
+++ source/js/modules/modals/init-sliders.js
@@ -2,25 +2,25 @@ import Swiper from '../../vendor/swiper-bundle';
 
 export function initSlider(target) {
   let swiperSlider = new Swiper(target, {
-    slidesPerView: 4,
-    spaceBetween: 40,
+    slidesPerView: 1,
+    spaceBetween: 20,
     loop: true,
     navigation: {
       nextEl: `${target}-button-next`,
       prevEl: `${target}-button-prev`,
     },
     breakpoints: {
-      768: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      1200: {
+      566: {
         slidesPerView: 2,
         spaceBetween: 30,
       },
-      1300: {
+      1066: {
         slidesPerView: 3,
         spaceBetween: 30,
+      },
+      1160: {
+        slidesPerView: 4,
+        spaceBetween: 40,
       },
     },
   });
